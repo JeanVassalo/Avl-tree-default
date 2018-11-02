126fbf00d137505f3e117182952491ec96e071ae
JeanVassalo <jeanvassaloferreira@gmail.com>
2018-11-02 20:33:41 -0300

    all files

---
+++ 000-AVL_Library.h
@@ -0,0 +1,27 @@
+/*	AVL Tree Library.
+	   Written by: 
+	   Date:       
+*/
+#include "stdbool.h"
+
+//	Text Include Files
+#include "00-Estrutura_AVL.h"		// AVL Tree Declarations
+#include "01-AVL_Create.h"		// AVL Tree Create
+#include "02-AVL_Insert.h"		// Application AVL Insert Function
+#include "03- _insert.h"		// AVL Internal Insert Function
+#include "04-insLeftBal.h"		// AVL Tree Left Balance
+#include "05-insRightBal.h"		// AVL Tree Right Balance
+#include "06-rotateLeftRight.h"		// AVL Tree Rotate Left and Right
+#include "07-AVL_Delete.h"		// AVL Tree Delete Interface
+#include "08-_delete.h"		// AVL Tree Internal Delete
+#include "09- dltRightBal.h"		// AVL Tree Delete Right Balance
+#include "10-dltLeftBal.h"		// AVL Tree Delete Left Balance
+#include "11-AVL_Retrieve.h"		// AVL Retrieve Data
+#include "12-_retrieve.h"		// AVL Tree Internal Retrieve Function
+#include "13-AVL_Traverse.h"		// Traverse AVL Tree
+#include "14-_traversal.h"		// Traverse AVL Tree Internal Function
+#include "15-AVL_Empty.h"		// AVL Empty Tree
+#include "16-AVL_Full.h"		// AVL Full Tree
+#include "17-AVL_Count.h"	    // AVL Tree Count
+#include "18-AVL_Destroy.h"		// Destroy Tree
+#include "19-_destroy.h"		// Recursive Destroy Tree
